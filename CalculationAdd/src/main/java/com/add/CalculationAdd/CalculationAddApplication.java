97364b5ee4339d02be066ad7e57863229e190ffe
AnastasiaPopova <anastasia.wuj@gmail.com>
2024-08-05 11:08:16 +0300

    add dependencies

---
+++ CalculationAdd/src/main/java/com/add/CalculationAdd/CalculationAddApplication.java
@@ -0,0 +1,13 @@
+package com.add.CalculationAdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationAddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationAddApplication.class, args);
+	}
+
+}
---
+++ CalculationAdd/src/test/java/com/add/CalculationAdd/CalculationAddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.add.CalculationAdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationAddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
