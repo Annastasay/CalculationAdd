a1bb6aec39a756985e4056eaacefd0548d94470c
”Popova <anastasia.a.popova@raiffeisen.ru>
2024-08-20 16:43:23 +0300

    add finding user by id

--- CalculationAdd/src/main/java/com/add/CalculationAdd/Service/UserService.java
+++ CalculationAdd/src/main/java/com/add/CalculationAdd/Service/UserService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -16,9 +18,19 @@ public class UserService {
     private final UserRepository userRepository;
     private final CreateRequestToEntityMapper mapper;
 
-@Transactional
-    public UserResponse createUser(CreateUserRequest request){
+    @Transactional
+    public UserResponse createUser(CreateUserRequest request) {
         UserEntity entity = mapper.createUserRequestToEntity(request);
         return mapper.entityToResponse(userRepository.save(entity));
+    }
+
+    @Transactional
+    public UserResponse findById(Long id) {
+        Optional<UserEntity> entity = userRepository.findById(id);
+        if(entity.isPresent()){
+            return mapper.entityToResponse(entity.get());
+        } else {
+            throw new NullPointerException("Пользователь с таким id не найден!");
+        }
     }
 }
