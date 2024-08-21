23523b02c04f45b74b902ddd97491670331da5e2
Anastasiya Popova <78546999+Annastasay@users.noreply.github.com>
2024-08-21 11:36:14 +0300

    add update and delete method (#2)
    Co-authored-by: ”Popova <anastasia.a.popova@raiffeisen.ru>

--- CalculationAdd/src/main/java/com/add/CalculationAdd/Service/UserService.java
+++ CalculationAdd/src/main/java/com/add/CalculationAdd/Service/UserService.java
@@ -2,6 +2,7 @@ package com.add.CalculationAdd.Service;
 
 import com.add.CalculationAdd.Mapper.CreateRequestToEntityMapper;
 import com.add.CalculationAdd.Model.CreateUserRequest;
+import com.add.CalculationAdd.Model.UpdateUserRequest;
 import com.add.CalculationAdd.Model.UserEntity;
 import com.add.CalculationAdd.Model.UserResponse;
 import com.add.CalculationAdd.Repository.UserRepository;
@@ -27,8 +28,31 @@ public class UserService {
     @Transactional
     public UserResponse findById(Long id) {
         Optional<UserEntity> entity = userRepository.findById(id);
-        if(entity.isPresent()){
+        if (entity.isPresent()) {
             return mapper.entityToResponse(entity.get());
+        } else {
+            throw new NullPointerException("Пользователь с таким id не найден!");
+        }
+    }
+
+    @Transactional
+    public String deleteUser(Long id) {
+        Optional<UserEntity> deletedEntity = userRepository.findById(id);
+        if (deletedEntity.isPresent()) {
+            userRepository.delete(deletedEntity.get());
+            return "Запись удалена";
+        } else {
+            return "Пользователь с таким id не найден!";
+        }
+    }
+
+    @Transactional
+    public UserResponse updateUser(Long id, UpdateUserRequest request) {
+        Optional<UserEntity> updatedEntity = userRepository.findById(id);
+        if (updatedEntity.isPresent()) {
+            UserEntity entity = updatedEntity.get();
+            entity.setNickname(request.getNickname());
+            return mapper.entityToResponse(entity);
         } else {
             throw new NullPointerException("Пользователь с таким id не найден!");
         }
