ac55e10ab4d6ce9a94ef9e0152c5b2f780ee34f4
”Popova <anastasia.a.popova@raiffeisen.ru>
2024-08-21 11:35:33 +0300

    add update and delete method

--- CalculationAdd/src/main/java/com/add/CalculationAdd/Controller/UserController.java
+++ CalculationAdd/src/main/java/com/add/CalculationAdd/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.add.CalculationAdd.Controller;
 
 import com.add.CalculationAdd.Model.CreateUserRequest;
+import com.add.CalculationAdd.Model.UpdateUserRequest;
 import com.add.CalculationAdd.Model.UserResponse;
 import com.add.CalculationAdd.Service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -8,19 +9,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping( "/api/user")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public UserResponse createUser(@RequestBody  CreateUserRequest request){
+    public UserResponse createUser(@RequestBody CreateUserRequest request) {
         return userService.createUser(request);
     }
 
     @GetMapping("/get")
-    public UserResponse getUser(@RequestParam Long id){
+    public UserResponse getUser(@RequestParam Long id) {
         return userService.findById(id);
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteUser(@RequestParam Long id) {
+        return userService.deleteUser(id);
+    }
+
+    @PutMapping("/update")
+    public UserResponse updateUser(
+            @RequestParam Long id,
+            @RequestBody UpdateUserRequest request
+    ) {
+        return userService.updateUser(id, request);
     }
 }
---
+++ CalculationAdd/src/main/java/com/add/CalculationAdd/Model/UpdateUserRequest.java
@@ -0,0 +1,13 @@
+package com.add.CalculationAdd.Model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateUserRequest {
+    private String nickname;
+}
