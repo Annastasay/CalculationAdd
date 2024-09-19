4402a6e2018a826f3b956f28a0b8e4fc44dcbb7c
”Popova <anastasia.a.popova@raiffeisen.ru>
2024-09-19 18:21:55 +0300

    add new microservice

--- CalculationAdd/src/main/java/com/add/CalculationAdd/controller/HeroController.java
+++ CalculationAdd/src/main/java/com/add/CalculationAdd/controller/HeroController.java
@@ -1,9 +1,10 @@
 package com.add.CalculationAdd.controller;
 
-
 import com.add.CalculationAdd.feign.PersonDTO;
 import com.add.CalculationAdd.feign.StarWarsFeignClient;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- CalculationAdd/src/main/java/com/add/CalculationAdd/feign/StarWarsFeignClient.java
+++ CalculationAdd/src/main/java/com/add/CalculationAdd/feign/StarWarsFeignClient.java
@@ -2,10 +2,9 @@ package com.add.CalculationAdd.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "hero", url = "http://localhost:8080/api/feign")
+@FeignClient(value = "hero", url = "/api/feign")
 public interface StarWarsFeignClient {
     @GetMapping("/getHero")
     public PersonDTO getPerson(@RequestParam String id);
