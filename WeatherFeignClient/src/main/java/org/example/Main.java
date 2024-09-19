14f7f19f8f4b5b84b79b0add183897554705e402
say <78546999+Annastasay@users.noreply.github.com>
2024-09-19 13:48:37 +0300

    Weather feign (#5)
    * add new microservice
    
    * add new microservice
    
    * move some classes
    
    ---------
    
    Co-authored-by: ”Popova <anastasia.a.popova@raiffeisen.ru>

--- WeatherFeignClient/src/main/java/org/example/Main.java
+++ WeatherFeignClient/src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableFeignClients
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
